48968cd6f192ed8b14915a5ec0c650b7a4c0b087
pexmee <pexmee@gmail.com>
2021-01-14 19:16:37 +0100

    Fixed the object.

--- message_generator/famous_quotes.js
+++ message_generator/famous_quotes.js
@@ -5,9 +5,11 @@
 
 
 const randMess = () => {
-    const container = {
-        0 : caesarQuote = {
-            quotes: caesar = [
+
+    const quoteObj = {
+        caesar: {
+            name: "Julius Caesar",
+            quotes: [
                 "Men in general are quick to believe that which they wish to be true.",
                 "Experience is the teacher of all things",
                 "Veni, vidi, vici.",
@@ -18,13 +20,11 @@ const randMess = () => {
                 "It is better to suffer once than to be in perpetual apprehension.",
                 "Great things should be done without hesitation so that the feeling of danger would not weaken the courage and speed.",
                 "Which death is preferably to every other? 'The unexpected.'"
-            ],
-            present (){
-                return this.quotes[Math.floor(Math.random()*caesar.length)] + "\n-Julius Caesar";
-            }
+            ]
         },
-        1 : hawkingQuote = {
-            quotes: hawking = [
+        hawkings: {
+            name: "Stephen Hawking",
+            quotes: [
                 "Women. They are a complete mystery.",
                 "Quiet people have the loudest minds.",
                 "However difficult life may seem, there is always something you can do, and succeed at. It matters that you don't just give up.",
@@ -36,13 +36,11 @@ const randMess = () => {
                 "People won't have time for you if you are always angry or complaining.",
                 "I'm not afraid of death, but I'm in no hurry to die.",
                 "Just because I do a lot of thinking doesn't mean I don't like parties and getting into trouble."
-            ],
-            present (){
-                return this.quotes[Math.floor(Math.random()*hawking.length)] + "\n-Stephen Hawking";
-            }
+            ]
         },
-        2 : jefferiesQuote = {
-            quotes: jefferies = [
+        jefferies: {
+            name: "Jim Jefferies",
+            quotes: [
                 "I'm not scared of dying, because I'm an atheist. I won't even know I'm dead. You know why? Because I'll be fuckin' dead.",
                 "On porn it seems very inviting, she seems to be having fun, but the thing is.. You can't smell porn.",
                 "If you don't drink you're a boring cunt and all of your stories suck.",
@@ -52,18 +50,17 @@ const randMess = () => {
                 "I could never be gay because I could never fuck someone I respect",
                 "The smart are always asking questions.. and that's like fucking cancer to religion.",
                 "Here's the good thing about people who believe in god.. They're idiots.",
-            ],
-            present (){
-                return this.quotes[Math.floor(Math.random()*jefferies.length)] + "\n-Jim Jefferies";
-            }
+            ]
         },
-        randQuote () {
-            const randNum = Math.floor(Math.random()*3); 
-            return this[randNum].present();
-        },
-    };
 
-    console.log(container.randQuote()); // Testing with node.js
+        present () {
+            const keys = Object.keys(this);
+            const randPerson = this[keys[Math.floor(Math.random()*3)]];
+            const randQuote = Math.floor(Math.random()*randPerson.quotes.length);  
+            return randPerson.quotes[randQuote] + `\n-${randPerson.name}`;
+        }
+    }
+    return quoteObj.present();
 };
 
-randMess(); // Just for testing purposes with node. 
+console.log(randMess()); // Just for testing purposes with node. 
